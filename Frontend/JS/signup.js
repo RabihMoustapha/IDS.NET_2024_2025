497fc42b9ecf8e8f6e098ec200ae912dc0610995
RabihMoustapha <mostapharabih59@gmail.com>
2025-02-11 10:31:42 +0200

    Refactor signup page: move inline styles to external CSS file, update form submission method, and implement JavaScript for user signup functionality

---
+++ Frontend/JS/signup.js
@@ -0,0 +1,33 @@
+async function Create() {
+    const username = document.getElementById("username");
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+
+    const userData = {
+        username: username.value,
+        email: email.value,
+        password: password.value
+    };
+
+    try {
+        const response = await fetch("https://localhost:7136/api/profiles", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+            body: JSON.stringify(userData)
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            alert("Signup successful!");
+            console.log(result);
+            localStorage.setItem("token", result.token);
+        } else {
+            alert("Signup failed. Please try again.");
+        }
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
